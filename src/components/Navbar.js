095780539208b2bdcf923a7b8bdaba695304afab
bgaunt2003 <bengaunt10@gmail.com>
2024-03-18 19:42:22 +0000

    sorted out app.js, routes and navbar component, neatened map

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import{ Link } from "react-router-dom"
+import three_bars_icon from '../three-bars-icon.jpg';
+import "../styles/nav.css"
+function Navbar() {
+
+    const [navOpen,  setnavOpen] = useState(false);
+
+    const openNav = () => {
+        setnavOpen(true);
+      }
+    
+      const closeNav = () => {
+        setnavOpen(false);
+      }
+
+  return (
+    <div className="Navbar">
+      <button className="open_button" onClick={openNav}><img src={three_bars_icon}/></button>
+      
+      <div className={navOpen ? "sidebar open": "sidebar"}>
+        <ul>
+          <a className='close_button' onClick={closeNav}>X</a>
+          <li><Link to="/"> Home </Link></li>
+          <li><Link to="Info"> Weather information </Link></li>
+          <li><Link to="Map"> Map </Link></li>
+        </ul>
+        </div>
+
+        
+
+
+    </div>
+  )
+}
+
+export default Navbar
--- src/components/map.js
+++ src/components/map.js
@@ -1,13 +1,13 @@
-// import React, { useState } from 'react';
+ import React from 'react';
 
 function BeachMap(searchTerm) {
 
 
   const mapUrl = `https://maps.google.com/maps?width=100%25&height=600&hl=en&q=beaches+in+${searchTerm}&t=&z=14&ie=UTF8&iwloc=B&output=embed`;
-
+  
   return (
     <div>
-      <iframe title="map" width="600" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src={mapUrl}/>
+      <iframe title="map" width="414" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src={mapUrl}/>
     </div>
     
    
